98dec71a9f532786043906a72c1e58436bb2e32f
James Forcier <james.forcier@coreos.com>
2017-06-09 10:58:14 -0700

    *: use rlibc instead of supplying LLVM intrinsics
    rlibc supplies all the necessary mem* functions, so don't supply our
    own.

--- src/uefi_entry/util.rs
+++ src/uefi_entry/util.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-extern crate uefi;
-
 use core::fmt::Arguments;
 
 extern "rust-intrinsic" {
@@ -37,17 +35,3 @@ pub fn rust_begin_panic(_fmt: Arguments, _file_line: &(&'static str, u32)) -> ! 
     }
 }
 
-// Needed for copy_nonoverlapping.
-#[no_mangle]
-pub extern "C" fn memcpy(dest: *mut u8, src: *const u8, n: usize) -> *mut u8 {
-    uefi::get_system_table()
-        .boot_services()
-        .copy_mem(dest, src, n)
-}
-
-#[no_mangle]
-pub extern "C" fn memset(buf: *mut u8, val: isize, size: usize) -> *const u8 {
-    uefi::get_system_table()
-        .boot_services()
-        .set_mem(buf, val as u8, size)
-}
