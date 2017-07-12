ec1d680cd4b8f46434de7dec5189c1249d30cd92
James Forcier <james.forcier@coreos.com>
2017-07-12 13:31:20 -0700

    util: remove usage of intrinsics
    Since intrinsics are feature-gated (making them nightly-only), removing
    usage of intrinsics is a step towards building on non-nightly Rust.

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -20,11 +20,6 @@ pub use self::input::*;
 
 use core::fmt::Arguments;
 
-extern "rust-intrinsic" {
-    pub fn transmute<T, U>(val: T) -> U;
-    pub fn abort() -> !;
-}
-
 #[lang = "eh_personality"]
 #[no_mangle]
 pub fn rust_eh_personality() {}
@@ -36,7 +31,5 @@ pub fn rust_eh_unwind_resume() {}
 #[lang = "panic_fmt"]
 #[no_mangle]
 pub fn rust_begin_panic(_fmt: Arguments, _file_line: &(&'static str, u32)) -> ! {
-    unsafe {
-        abort();
-    }
+    loop {}
 }
